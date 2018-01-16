b42c19a07c94cae13c1a9446c17bf034bcdc62c9
Nahid Shaiket <nahidshaiket10300@gmail.com>
2018-01-16 20:59:04 +0600

    Removed the wrong datatables package and installed the correct one

---
+++ app/DataTables/MoviesDataTable.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\DataTables;
+
+use App\User;
+use Yajra\DataTables\Services\DataTable;
+
+class MoviesDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables($query)
+            ->addColumn('action', 'movies.action');
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\User $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(User $model)
+    {
+        return $model->newQuery()->select('id', 'add-your-columns-here', 'created_at', 'updated_at');
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->addAction(['width' => '80px'])
+                    ->parameters($this->getBuilderParameters());
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            'id',
+            'add your columns',
+            'created_at',
+            'updated_at'
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'Movies_' . date('YmdHis');
+    }
+}
--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
-//        Yajra\DataTables\DataTablesServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -203,7 +203,6 @@ return [
         'Config' => Illuminate\Support\Facades\Config::class,
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
         'Crypt' => Illuminate\Support\Facades\Crypt::class,
-        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
         'DB' => Illuminate\Support\Facades\DB::class,
         'Eloquent' => Illuminate\Database\Eloquent\Model::class,
         'Event' => Illuminate\Support\Facades\Event::class,
