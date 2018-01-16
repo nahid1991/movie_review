e3e594a8a5682cfef707ac4fb4b034ddc0c49381
Nahid Shaiket <nahidshaiket10300@gmail.com>
2018-01-16 15:30:04 +0600

    Added: casting to see if user is admin or not

--- app/User.php
+++ app/User.php
@@ -27,6 +27,10 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    protected $casts = [
+        'is_admin' => 'boolean',
+    ];
+
     public function isAdmin() {
         return $this->admin;
     }
