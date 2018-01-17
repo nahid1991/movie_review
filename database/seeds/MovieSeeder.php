ca4748628ee7e75b5b0b65383757e89d8dcabfd7
Nahid Shaiket <nahidshaiket10300@gmail.com>
2018-01-17 16:51:34 +0600

    changed movie seeder data

--- database/seeds/MovieSeeder.php
+++ database/seeds/MovieSeeder.php
@@ -16,7 +16,7 @@ class MovieSeeder extends Seeder
         DB::table('movies')->insert([
             'title' => 'Movie#1',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'cover_image' => 'https://i.pinimg.com/736x/fd/5e/66/fd5e662dce1a3a8cd192a5952fa64f02--classic-poster-classic-movies-posters.jpg',
+            'cover_image' => 'movie_poster.jpg',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
@@ -24,7 +24,7 @@ class MovieSeeder extends Seeder
         DB::table('movies')->insert([
             'title' => 'Movie#2',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'cover_image' => 'https://i.pinimg.com/736x/fd/5e/66/fd5e662dce1a3a8cd192a5952fa64f02--classic-poster-classic-movies-posters.jpg',
+            'cover_image' => 'movie_poster.jpg',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
@@ -32,7 +32,7 @@ class MovieSeeder extends Seeder
         DB::table('movies')->insert([
             'title' => 'Movie#3',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'cover_image' => 'https://i.pinimg.com/736x/fd/5e/66/fd5e662dce1a3a8cd192a5952fa64f02--classic-poster-classic-movies-posters.jpg',
+            'cover_image' => 'movie_poster.jpg',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
